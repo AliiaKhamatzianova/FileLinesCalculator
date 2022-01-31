0935cf4d8f4325338839b54b7c71c83c216b4a39
AliiaKhamatzianova <aliiakhamatzianova@outlook.com>
2022-01-31 16:09:49 +0300

    Add files via upload
    Implement a program that calcualtes the total number of lines in all files in a given directory. Assume that files
    found in the given directory are text files.
    Usage example:
    linecounter &lt;path-to-a-directory&gt;
    14124
    Requirements:
     Must be written in C++
     Can use third party libraries if needed (i.e. Boost)
     Must utilise all available CPU cores

---
+++ TestThread/TestThread/filelinescalculator.cpp
@@ -0,0 +1,87 @@
+#include "filelinescalculator.h"
+
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+
+
+using namespace std::filesystem;
+
+std::mutex FileLinesCalculator::m_mutex;
+
+FileLinesCalculator::FileLinesCalculator(const std::string &fileDirectory):
+    m_fileDirectory(fileDirectory)
+    ,m_countOfLines(0)
+	,m_countRunThread(0)
+{
+	m_threadCount = std::thread::hardware_concurrency();
+	path filePath(fileDirectory);
+	if (!is_directory(filePath)) {
+		//calculateLine(fileDirectory);
+		m_vecOfFiles.push_back(fileDirectory);
+	}
+	else {
+		for (auto const& dir_entry : recursive_directory_iterator(filePath))
+		{
+			if (!is_directory(dir_entry.path())) {
+				//calculateLine(dir_entry.path().filename().u8string());
+				m_vecOfFiles.push_back(dir_entry.path().filename().u8string());
+			}
+		}
+
+	}
+	start();
+
+}
+
+void FileLinesCalculator::printResult()
+{
+	std::cout << "m_countOfLines = " << m_countOfLines;
+}
+
+void FileLinesCalculator::start()
+{
+	for (auto i = 0; i < m_threadCount - 1; i++) {
+		std::thread thread(&FileLinesCalculator::startCalculate, this);
+		m_countRunThread++;
+		thread.join();
+	}
+
+	startCalculate();
+}
+
+void FileLinesCalculator::calculateLine(const std::string &fileName)
+{
+	path filePath(fileName);
+	int counter = 0;
+	std::ifstream fstream(filePath);
+	std::string str(static_cast<size_t>(file_size(filePath)),'0');
+	while (!fstream.eof()) {
+		fstream.getline(str.data(), static_cast<size_t>(file_size(filePath)), '\n');
+		counter++;
+	}
+	m_countOfLines += counter;
+	
+}
+
+void FileLinesCalculator::startCalculate()
+{
+	std::string filePath;
+	bool flag = true;
+	while (flag) {
+		m_mutex.lock();
+		if(!m_vecOfFiles.empty()) {
+			filePath = m_vecOfFiles.back();
+			m_vecOfFiles.pop_back();
+			m_mutex.unlock();
+			calculateLine(filePath);
+		} else {
+			m_mutex.unlock();
+			flag = false;
+			m_countRunThread--;
+		}
+	}
+	if (m_vecOfFiles.empty() && m_countRunThread == 0) {
+		printResult();
+	}
+}
---
+++ TestThread/TestThread/filelinescalculator.h
@@ -0,0 +1,30 @@
+#ifndef FILELINESCALCULATOR_H
+#define FILELINESCALCULATOR_H
+
+#include <string>
+#include <atomic>
+#include <thread>
+#include <mutex>
+#include <vector>
+
+class FileLinesCalculator
+{
+public:
+    FileLinesCalculator(const std::string& fileDirectory);
+
+
+private:
+    std::string m_fileDirectory;
+    std::atomic_int m_countOfLines;
+	unsigned int m_threadCount;
+	std::vector<std::string> m_vecOfFiles;
+	static std::mutex m_mutex;
+	std::atomic_int m_countRunThread;
+
+    void printResult();
+	void start();
+    void calculateLine(const std::string& fileName);
+	void startCalculate();
+};
+
+#endif // FILELINESCALCULATOR_H
